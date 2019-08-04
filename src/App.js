6b578362cce2d1cb4045d38fdab7b40e0067000f
Mohamed <mohamedadham27@gmail.com>
2019-08-04 15:29:48 +0200

    update css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import TodoItem from './Todolist.js'
     
 class App extends React.Component {
@@ -16,7 +17,7 @@ class App extends React.Component {
         
       const{todoText,todoData,id}=this.state
       this.setState({id:this.state.id+1})
-        todoData.push({text:todoText,completed:false,id:this.state.id})
+        todoData.push({text:todoText,completed:false,id:this.state.id,italic:false})
         this.setState({todoData:todoData})
         
     }
@@ -48,6 +49,37 @@ class App extends React.Component {
         
     }
     
+    delete=(id)=>{
+        let i= 0;
+        this.setState((prevState)=>{
+          const updatedTodos= prevState.todoData.map(todo=>{
+            if(todo.id===id){
+          return  prevState.todoData.splice(i,1)
+        }
+           i++;           
+        })
+          
+        return {
+               updatedTodos
+            }
+        })
+    }
+    
+    italic=(id)=>{
+        console.log('itaaaaalic')
+        this.setState(prevState=>{
+            const updatedTodos=prevState.todoData.map(todo=>{
+                if(todo.id===id){
+                   todo.italic=!todo.italic
+                }
+                return todo
+            })
+            return{
+                todoData:updatedTodos
+            }
+        })
+        
+    }
     render(){
       const{todoText,completed,todoData,id}=this.state;
         
@@ -63,7 +95,7 @@ class App extends React.Component {
                 <button>  Add </button>
             </form>
              <div className="todo-items">
-           { todoData.map(item=><TodoItem item={item} handleCheckbox={this.handleCheckbox} key={item.id}/>) }
+           { todoData.map(item=><TodoItem item={item} handleCheckbox={this.handleCheckbox} delete={this.delete} italic={this.italic} key={item.id}/>) }
             
             </div>
             </div>
--- src/Todolist.js
+++ src/Todolist.js
@@ -1,21 +1,32 @@
-import React from 'react' 
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 const styles={
     textDecoration:'line-through',
     
 }
+const italic={
+    fontStyle:'italic'
+}
 
 function TodoItem(props){
-   
+   console.log(props.item.completed)
     
     return(
         <div className="todo-list">
         
             
-            <p className="todo-text" style={props.item.completed? styles:null}> {props.item.text}</p>
-            <input   style={{marginRight:'0vmin',width:'10%'}} type="checkbox" checked={props.item.completed}  onChange={ ()=>props.handleCheckbox(props.item.id)}/> 
+            <p className="todo-text" style={{textDecoration:props.item.completed?'line-through':null, fontStyle:props.item.italic?'italic':null}}> {props.item.text}</p>
         
-       
+        <div className="option">
+            <input   style={{marginRight:'0vmin'}} type="checkbox" checked={props.item.completed}  onChange={ ()=>props.handleCheckbox(props.item.id)}/> 
+            <FontAwesomeIcon style={{color:'black'}} icon={faStar} onClick={()=>props.italic(props.item.id)}/>
+            <FontAwesomeIcon style={{color:'black'}} icon={faTrashAlt} onClick={()=>props.delete(props.item.id)}/>
+          
+        
+       </div>
         </div>
     
     )
